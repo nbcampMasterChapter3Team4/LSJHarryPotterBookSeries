b29c73682b0f88fba12a1454f641d4330fe4f918
Yimkeul <leesjun29@gmail.com>
2025-03-29 19:40:55 +0900

    fix : 변수명 추가 변경

--- HarryPotterBookSeries/HarryPotterBookSeries/View/ViewComponents/BookTitleAndSeriesView.swift
+++ HarryPotterBookSeries/HarryPotterBookSeries/View/ViewComponents/BookTitleAndSeriesView.swift
@@ -10,28 +10,28 @@ import SnapKit
 import Then
 
 final class BookTitleAndSeriesView: UIView {
-    
+
     func configure(index: Int, book: Attributes) {
         titleLabel.text = book.title
         seriesNumberLabel.setTitle("\(index + 1)", for: .normal)
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private let titleLabel = UILabel().then {
         $0.textColor = .label
         $0.font = .systemFont(ofSize: 24, weight: .bold)
         $0.numberOfLines = 2
         $0.textAlignment = .center
     }
-    
+
     private let seriesNumberLabel = UIButton().then {
         $0.setTitleColor(.white, for: .normal)
         $0.titleLabel?.font = .systemFont(ofSize: 16)
@@ -41,19 +41,22 @@ final class BookTitleAndSeriesView: UIView {
         $0.titleLabel?.textAlignment = .center
     }
     
+    
+}
 
+
+extension BookTitleAndSeriesView {
     
     private func setupView() {
-        
         addSubview(titleLabel)
         addSubview(seriesNumberLabel)
-        
+
         titleLabel.snp.makeConstraints { make in
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.top.equalToSuperview().offset(10)
         }
-        
+
         seriesNumberLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(16)
             make.centerX.equalToSuperview()
--- HarryPotterBookSeries/HarryPotterBookSeries/View/ViewController.swift
+++ HarryPotterBookSeries/HarryPotterBookSeries/View/ViewController.swift
@@ -16,15 +16,15 @@ class ViewController: UIViewController {
     private let viewModel = DataServiceViewModel() // ✅ ViewModel 인스턴스 생성
     private let disposeBag = DisposeBag() // ✅ Rx 메모리 관리용 DisposeBag
 
-    private let bookTitleAndSeries = BookTitleAndSeriesView()
-    private let bookInfoArea = BookInfoView()
-    private let bookDedicationAndSummary = BookDedicationAndSummaryView()
-    private let bookChaptersArea = BookChaptersView()
+    private let bookTitleAndSeriesView = BookTitleAndSeriesView()
+    private let bookInfoView = BookInfoView()
+    private let bookDedicationAndSummaryView = BookDedicationAndSummaryView()
+    private let bookChaptersView = BookChaptersView()
 
     private let scrollView = UIScrollView().then {
         $0.showsVerticalScrollIndicator = false // ✅ 스크롤 바 숨김
     }
-    
+
     private let contentView = UIView() // ✅ scrollView 내부 컨텐츠 뷰
 
     override func viewDidLoad() {
@@ -34,51 +34,57 @@ class ViewController: UIViewController {
         setupBindings()
         viewModel.loadBooks() // ✅ 데이터 로드
     }
+}
 
+extension ViewController {
 
     private func setupUI() {
-        self.view.addSubview(bookTitleAndSeries)
+        self.view.addSubview(bookTitleAndSeriesView)
         self.view.addSubview(scrollView)
-        
+
         scrollView.addSubview(contentView)
-        contentView.addSubview(bookInfoArea)
-        contentView.addSubview(bookDedicationAndSummary)
-        contentView.addSubview(bookChaptersArea)
-        
-        bookTitleAndSeries.snp.makeConstraints { make in
+        contentView.addSubview(bookInfoView)
+        contentView.addSubview(bookDedicationAndSummaryView)
+        contentView.addSubview(bookChaptersView)
+
+        bookTitleAndSeriesView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.leading.trailing.equalToSuperview()
         }
-        
+
         scrollView.snp.makeConstraints { make in
-            make.top.equalTo(bookTitleAndSeries.snp.bottom)
+            make.top.equalTo(bookTitleAndSeriesView.snp.bottom)
             make.leading.trailing.bottom.equalToSuperview()
         }
-        
+
         contentView.snp.makeConstraints { make in
             make.top.leading.bottom.trailing.equalToSuperview()
             make.width.equalTo(scrollView.snp.width)
         }
 
-        bookInfoArea.snp.makeConstraints { make in
+        bookInfoView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(16)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(5)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-5)
         }
-        
-        bookDedicationAndSummary.snp.makeConstraints { make in
-            make.top.equalTo(bookInfoArea.snp.bottom).offset(24)
+
+        bookDedicationAndSummaryView.snp.makeConstraints { make in
+            make.top.equalTo(bookInfoView.snp.bottom).offset(24)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
         }
-        
-        bookChaptersArea.snp.makeConstraints { make in
-            make.top.equalTo(bookDedicationAndSummary.snp.bottom).offset(24)
+
+        bookChaptersView.snp.makeConstraints { make in
+            make.top.equalTo(bookDedicationAndSummaryView.snp.bottom).offset(24)
             make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
             make.bottom.equalToSuperview()
         }
     }
+
+}
+
+extension ViewController {
 
     private func setupBindings() {
         viewModel.books
@@ -88,10 +94,10 @@ class ViewController: UIViewController {
 
             for (index, book) in books.enumerated() {
                 if index == 0 {
-                    self.bookTitleAndSeries.configure(index: index, book: book)
-                    self.bookInfoArea.configure(index: index, book: book)
-                    self.bookDedicationAndSummary.configure(book: book)
-                    self.bookChaptersArea.configure(book: book.chapters.map { $0.title })
+                    self.bookTitleAndSeriesView.configure(index: index, book: book)
+                    self.bookInfoView.configure(index: index, book: book)
+                    self.bookDedicationAndSummaryView.configure(book: book)
+                    self.bookChaptersView.configure(book: book.chapters.map { $0.title })
                 }
             }
         })
